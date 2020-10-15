c4bb6c1b2df23139a5e18cb7f571aaaf4abf5c01
phillipfriedelj <40246050+phillipfriedelj@users.noreply.github.com>
2020-10-15 16:50:35 +0200

    Improved the adding functionalty by using vue functionalities and minor refactoring

--- homeworks/homework-1/src/models/news-item.model.ts
+++ homeworks/homework-1/src/models/news-item.model.ts
@@ -3,7 +3,15 @@ export class NewsItemModel {
   title: string;
   votes: number;
 
-  constructor(id: number, title: string, votes: number) {
+  constructor({
+    id,
+    title,
+    votes = 0
+  }: {
+    id: number;
+    title: string;
+    votes: number;
+  }) {
     this.id = id;
     this.title = title;
     this.votes = votes;
