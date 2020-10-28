e696bd2c19742094ef5be16b97eccd18a090dff3
Christoph Stach <christoph.stach@gmail.com>
2020-10-29 01:28:26 +0800

    Downgrade to vue 2

--- homeworks/homework-1/src/shims-vue.d.ts
+++ homeworks/homework-1/src/shims-vue.d.ts
@@ -1,6 +1,4 @@
-declare module '*.vue' {
-  // eslint-disable-next-line 
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent
-  export default component
+declare module "*.vue" {
+  import Vue from "vue";
+  export default Vue;
 }
