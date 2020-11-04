aa7f82abf05f9aca96fa6fe7ea8d4aac8b4bd480
FlorianTh2 <s0558101@htw-berlin.de>
2020-11-04 09:33:34 +0100

    Removed first test to merge value 2 into master with vue 3

--- homeworks/homework-1/tests/unit/news-list.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from "@vue/test-utils";
-import NewsList from "@/components/NewsList.vue";
-
-describe("NewsList.vue", () => {
-  it("renders news list string", () => {
-    const wrapper = mount(NewsList);
-    expect(wrapper.text()).toMatch("News List");
-  });
-});
