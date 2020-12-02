da5d9e1a2b69646d41d20117b0054a8e6d9736fd
FlorianTh2 <s0558101@htw-berlin.de>
2020-12-02 19:27:15 +0100

    Adjusted test, last test is failing since wrong dataSource

--- backend/src/datasources/posts.spec.ts
+++ backend/src/datasources/posts.spec.ts
@@ -5,6 +5,7 @@ import { users } from "../seed-data";
 import PostDatasource from "./post.datasource";
 import UserDatasource from "./user.datasource";
 import User from "../entities/user.entity";
+import { JwtPayload } from "../jwt-payload";
 
 let postDb = new PostDatasource();
 const userDb = new UserDatasource();
@@ -12,12 +13,27 @@ const dataSource = { postDatasource: postDb, userDatasource: userDb };
 userDb.users = users;
 postDb.posts = [];
 
-const serverInitializer = new ServerInitializer();
-const server = serverInitializer.createServer({
-  dataSources: () => dataSource,
-});
 
-const { query, mutate } = createTestClient(server);
+
+
+const serverInitializer = new ServerInitializer();
+const serverUnauthorized = serverInitializer.createServer({
+  dataSources: () => dataSource
+}
+);
+
+const serverAuthorized = serverInitializer.createServer({
+  dataSources: () => dataSource,
+  context: () => {
+    return {
+      user: {
+        id: "58334916-ae55-4149-add5-0bc11f1b43c6",
+      } as Partial<JwtPayload>
+    }
+  },
+}
+);
+
 
 describe("queries", () => {
   describe("POSTS", () => {
@@ -31,6 +47,7 @@ describe("queries", () => {
     `;
 
     it("returns empty array", async () => {
+      const { query, mutate } = createTestClient(serverUnauthorized);
       await expect(query({ query: POSTS })).resolves.toMatchObject({
         errors: undefined,
         data: { posts: [] },
@@ -50,6 +67,7 @@ describe("queries", () => {
       });
 
       it("returns posts", async () => {
+        const { query, mutate } = createTestClient(serverUnauthorized);
         await expect(query({ query: POSTS })).resolves.toMatchObject({
           errors: undefined,
           data: {
@@ -76,6 +94,7 @@ describe("queries", () => {
     `;
 
     it("returns all users", async () => {
+      const { query, mutate } = createTestClient(serverUnauthorized);
       await expect(query({ query: USERS })).resolves.toMatchObject({
         errors: undefined,
         data: {
@@ -98,15 +117,12 @@ describe("mutations", () => {
   });
 
   describe("WRITE_POST", () => {
-    const action = () =>
+    const action = (mutate) =>
       mutate({
         mutation: WRITE_POST,
         variables: {
           post: {
             title: "Some post",
-            author: {
-              name: "Some author",
-            },
           },
         },
       });
@@ -118,19 +134,24 @@ describe("mutations", () => {
         }
       }
     `;
-    it("responds with created post", async () => {
-      // mutation{
-      //     write(post: {
-      //         title: "test1",
-      //         author: {
-      //         name: "test1"
-      //         }
-      //     }){
-      //         id
-      //     }
-      //     }
 
-      await expect(action()).resolves.toMatchObject({
+    it("responds Not Authorised since no jwt", async () => {
+      const { query, mutate } = createTestClient(serverUnauthorized);
+      await expect(action(mutate)).resolves.toMatchObject({
+        errors: [
+          // https://jestjs.io/docs/en/expect#expectobjectcontainingobject
+          expect.objectContaining({message: "Not Authorised!"})
+        ],
+        data: {
+          write: null,
+        },
+      });
+    });
+
+
+    it("responds with created post", async () => {
+      const { query, mutate } = createTestClient(serverAuthorized);
+      await expect(action(mutate)).resolves.toMatchObject({
         errors: undefined,
         data: {
           write: {
@@ -142,34 +163,22 @@ describe("mutations", () => {
     });
 
     const UPVOTE_VOTE = gql`
-      mutation($id: ID!, $voter: UserInput!) {
-        upvote(id: $id, voter: $voter) {
+      mutation($id: ID!) {
+        upvote(id: $id) {
           votes
         }
       }
     `;
-    const upvote = (id, name) =>
+
+    const upvote = (id, mutate) =>
       mutate({
         mutation: UPVOTE_VOTE,
         variables: {
-          id,
-          voter: {
-            name,
-          },
+          id
         },
       });
 
     it("upvote a post", async () => {
-      // mutation{
-      //     upvote(id: "0903cadb-1330-4980-b117-cc54888eae54", voter: {
-      //       name: "Christoph Stach"
-      //     })
-      //     {
-      //       id
-      //       title
-      //     }
-      //   }
-
       const postSample = {
         id: "1",
         title: "Ein Toller Title",
@@ -180,8 +189,11 @@ describe("mutations", () => {
       expect(postDb.posts.length).toBe(1);
       expect(postDb.posts[0].voters.length).toBe(0);
 
-      const votes = await upvote(postSample.id, postSample.id);
+      const { query, mutate } = createTestClient(serverAuthorized);
 
+      const votes = await upvote("7ed8828b-f4de-4359-8160-1df1ff3234cd", mutate);
+
+      console.log("\n\n\n\n\n", votes);
       expect(votes.data.upvote.votes).toBe(1);
     });
   });
